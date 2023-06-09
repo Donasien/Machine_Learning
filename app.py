c3a0028d9511b6dd6f616e66f6473eb08b612ae1
amienramdhani <mar081201@gmail.com>
2023-06-09 10:20:05 +0700

    Maintenance App.py API Lung and Alzheimer

--- app.py
+++ app.py
@@ -18,23 +18,23 @@ def prepare_image(img):
     img = np.expand_dims(img, 0)
     return img
 
-def prepare_images(img):
-    img = Image.open(io.BytesIO(img)).convert('RGB')
-    img = img.resize((224, 224))
-    img = np.array(img)
-    img = np.expand_dims(img, 0)
-    return img
+def prepare_images(imgs):
+    imgs = Image.open(io.BytesIO(imgs)).convert('RGB')
+    imgs = imgs.resize((224, 224))
+    imgs = np.array(imgs)
+    imgs = np.expand_dims(imgs, 0)
+    return imgs
 
 def predict_result_alzheimer(img):
     return "Pneumonia" if model.predict(img)[0][0] > 0.5 else "Normal"
 
 
-def predict_result_lung(img):
-    if models.predict(img)[0][0] > 0.5:
+def predict_result_lung(imgs):
+    if models.predict(imgs)[0][0] > 0.5:
         return "Benign"
-    elif models.predict(img)[0][1] > 0.5:
+    elif models.predict(imgs)[0][1] > 0.5:
         return  "Malignant"
-    elif models.predict(img)[0][2] > 0.5:
+    elif models.predict(imgs)[0][2] > 0.5:
         return "normal"
     else :
         return "CT-Scan Tidak Dapat di Prediksi"
@@ -42,7 +42,7 @@ def predict_result_lung(img):
 
 app = Flask(__name__)
 
-@app.route('/alzheimer', methods=['POST'])
+@app.route('/predict_disease', methods=['POST'])
 def infer_image():
     if 'file' not in request.files:
         return "Please try again. The Image doesn't exist"
@@ -54,24 +54,26 @@ def infer_image():
 
     img_bytes = file.read()
     img = prepare_image(img_bytes)
+    imgs = prepare_images(img_bytes)
 
     return jsonify(prediction=predict_result_alzheimer(img))
-
-
-@app.route('/lung', methods=['POST'])
-def infer_images():
-    if 'file' not in request.files:
-        return "Please try again. The Image doesn't exist"
-    
-    file = request.files.get('file')
-
-    if not file:
-        return
-
-    img_bytes = file.read()
-    img = prepare_images(img_bytes)
-
     return jsonify(prediction=predict_result_lung(img))
+
+
+# @app.route('/lung', methods=['POST'])
+# def infer_images():
+#     if 'file' not in request.files:
+#         return "Please try again. The Image doesn't exist"
+    
+#     file = request.files.get('file')
+
+#     if not file:
+#         return
+
+#     img_bytes = file.read()
+#     img = prepare_images(img_bytes)
+
+#     return jsonify(prediction=predict_result_lung(img))
     
 
 @app.route('/', methods=['GET'])
